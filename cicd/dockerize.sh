cd69ec2869837fe6417d6cf74dc2b4066d45036b
Travis Gengler <TravisAGengler@gmail.com>
2016-08-05 13:53:48 -0600

    Attempting to retrieve deployment scripts and using robust dockerize configuration with new Docker version. Added validateAndTest script

--- cicd/dockerize.sh
+++ cicd/dockerize.sh
@@ -3,10 +3,21 @@ APPSTAGE="$1"
 PROJSHA="$2"
 PROJBRANCH="${3//\//_}"
 PROJVER="`cat ../project-version.txt`"
+PROJTAGS="`git show-ref --tags |grep $PROJSHA |cut -d \/  -f 3- |tr / _ |xargs echo`"
 
 echo "Version: $PROJVER"
 echo "Commit: $PROJSHA"
 echo "Branch: $PROJBRANCH"
+echo "Tags:"
+for i in ${PROJTAGS[@]}
+do
+	echo "\t$i"
+done
+
+if [ "$PROJBRANCH" = "versioning" ]; then
+  echo "On versioning branch - skipping docker build."
+  exit 0
+fi
 
 if [ "$APPSTAGE" = "^" ]; then
   if [ "$PROJBRANCH" = "master" ]; then
@@ -18,9 +29,27 @@ if [ "$APPSTAGE" = "^" ]; then
   fi
 fi
 
-docker build -f cicd/Dockerfile -t apptomcat:local .
+docker build -f cicd/Dockerfile \
+  --build-arg APPSTAGE=$APPSTAGE \
+  --build-arg SRC_BRANCH_A=$PROJBRANCH \
+  --build-arg SRC_VER_A="$PROJVER" \
+  --build-arg SRC_COMMIT_A=$PROJSHA \
+  --build-arg SRC_TAGS_A="$PROJTAGS" \
+  -t apptomcat:local .
+
+docker tag apptomcat:local quay.io/byuoit/ical2json-rest:$PROJBRANCH
+[ "$4" != "skip" ] && docker push quay.io/byuoit/ical2json-rest:$PROJBRANCH
+
+docker tag apptomcat:local quay.io/byuoit/ical2json-rest:${PROJSHA:0:16}
+[ "$4" != "skip" ] && docker push quay.io/byuoit/ical2json-rest:${PROJSHA:0:16}
 
 if [ "$PROJBRANCH" = "master" ] || [ "$PROJBRANCH" = "dev" ]; then
   docker tag apptomcat:local quay.io/byuoit/ical2json-rest:$PROJVER
   [ "$4" != "skip" ] && docker push quay.io/byuoit/ical2json-rest:$PROJVER
 fi
+
+for i in ${PROJTAGS[@]}
+do
+	docker tag apptomcat:local quay.io/byuoit/ical2json-rest:$i
+	[ "$4" != "skip" ] && docker push quay.io/byuoit/ical2json-rest:$i
+done
---
+++ cicd/validateAndBuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PROJBRANCH="$1"
+
+SNAP=$(grep SNAPSHOT ../project-version.txt |wc -l)
+
+if [ "$PROJBRANCH" = "versioning" ]; then
+	echo "On versioning branch"
+	mvn clean install
+elif [ $SNAP -eq 1 ] && [ "$PROJBRANCH" = "master" ]; then
+	echo "SNAPSHOT version on master branch"
+	exit 1
+elif [ $SNAP -eq 1 ]; then
+	echo "SNAPSHOT version on other branch ($PROJBRANCH)"
+	mvn clean deploy
+elif [ "$PROJBRANCH" = "master" ]; then
+	echo "Non-SNAPSHOT version on master branch"
+	mvn clean deploy
+else
+	echo "Non-SNAPSHOT version on other branch ($PROJBRANCH)"
+	exit 1
+fi
