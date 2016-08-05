011e6c0024acc8507dfea9a7f7f459567e026318
Travis Gengler <TravisAGengler@gmail.com>
2016-08-05 14:10:31 -0600

    Changed maven goal from test to install to crete war

--- cicd/validateAndBuild.sh
+++ cicd/validateAndBuild.sh
@@ -9,7 +9,7 @@ if [ "$PROJBRANCH" = "versioning" ]; then
 	mvn clean install
 elif [ $SNAP -eq 1 ] && [ "$PROJBRANCH" = "master" ]; then
 	echo "SNAPSHOT version on master branch"
-	mvn clean test
+	mvn clean install
 elif [ $SNAP -eq 1 ]; then
 	echo "SNAPSHOT version on other branch ($PROJBRANCH)"
 	mvn clean deploy
